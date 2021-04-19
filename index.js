dcb068a7df30bdf2d17b398d9409e021176f16f9
Crystal <ceceherrera490@gmail.com>
2021-04-18 22:49:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(arr, string) {
+  const matchName = arr.filter(
+    (name) => name.toLowerCase() === string.toLowerCase()
+  );
+  return matchName;
+}
+
+function fuzzyMatch(arr, string) {
+  const provideLetters = arr.filter((lettrs) => lettrs[0] === string[0]);
+  return provideLetters;
+}
+
+function matchName(arr, string) {
+  let elementFind = arr.find((person) => person.name === string);
+  return elementFind;
+}
